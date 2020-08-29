607744b3a2226e21cefaeb1b8f4777ab6dcb3ef9
Cybo1927 <39284087+Cybo1927@users.noreply.github.com>
2020-08-29 15:41:43 +0000

    Update Inline Script Buffer.user.js

--- Inline Script Buffer.user.js
+++ Inline Script Buffer.user.js
@@ -1,13 +1,16 @@
 // ==UserScript==
 // @name         Inline Script Buffer
-// @version      1.0
+// @version      1.1
 // @homepage     https://github.com/Cybo1927/Userscripts/
 // @description  Let inline scripts load but then remove them to prevent them from making changes to the site after they've loaded. A lot of adult sites seem to use these inline scripts to make it so when you click anywhere an ad pops up
 // @author       Cybo1927
-// @run-at       document-start
+// @run-at       document-idle
 // @include      *://*porn*.*/*
+// @include      *://*.*/*porn*
 // @include      *://*hentai*.*/*
+// @include      *://*.*/*hentai*
 // @include      *://*sex*.*/*
+// @include      *://*.*/*sex*
 // @include      *://*xxx*.*/*
 // @include      *://*xvideo*.*/*
 // @include      *://*.xxx/*
