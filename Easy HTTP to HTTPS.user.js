6cca82d191905e29a514b3d077eab0de5a46c76d
Cybo1927 <39284087+Cybo1927@users.noreply.github.com>
2020-08-29 14:56:26 +0000

    Update Easy HTTP to HTTPS.user.js

--- Easy HTTP to HTTPS.user.js
+++ Easy HTTP to HTTPS.user.js
@@ -1,13 +1,23 @@
 // ==UserScript==
 // @name         Easy HTTP to HTTPS
-// @version      1.2
+// @version      1.3
 // @homepage     https://github.com/Cybo1927/Userscripts/blob/master/Easy%20HTTP%20to%20HTTPS.user.js
 // @description  Redirect HTTP to HTTPS
 // @author       Cybo1927
 // @run-at       document-start
-// @include      http://*/*
+// @include      *://*/*
 // @exclude      http://127.0.0.1/*
+// @exclude      http://192.168.*/*
 // @grant        none
 // ==/UserScript==
 
+if (location.protocol !== 'https:') {
+  
 window.location.protocol = 'https:';
+  console.log("Upgrading to secure protocol")
+  
+  } else {
+    
+  console.log("Site already using secure HTTPS protocol")
+    
+}
